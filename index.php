66d411c033ab027d89931b2c1bd091d1a60aa866
Diamond705 <n.diamond.705@gmail.com>
2017-07-08 19:34:19 +0100

    data attributes updated

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>D705-JS-Lightbox</title>
+
+    <!--stylesheets-->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.css" />
+    <link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css" />
+    <link rel="stylesheet" type="text/css" href="line-icons/line-icons.css" />
+    <link rel="stylesheet" href="css/js-lightbox.css">
+</head>
+
+<body>
+    <div class="expanded-grid hidden-xs">
+        <div class="row">
+            <div class="expanded-col-60">
+
+                <div class="expanded-col-1 expanded-carousel index0img" data-index="0" data-username="" data-imagetext="caption and comments"
+                    data-likes="0" data-dislikes="0" data-imagepath="img/img001.png" data-posturl="#">
+                </div>
+                <div class="expanded-col-1 expanded-carousel index1img" data-index="1" data-username="your-username" data-imagetext="caption and comments"
+                    data-likes="0" data-dislikes="0" data-imagepath="img/img002.png" data-posturl="#">
+                </div>
+                <div class="expanded-col-1 expanded-carousel index2img" data-index="2" data-username="your-username" data-imagetext="caption and comments"
+                    data-likes="0" data-dislikes="0" data-imagepath="img/img003.png" data-posturl="#">
+                </div>
+                <div class="expanded-col-1 expanded-carousel index3img" data-index="3" data-username="your-username" data-imagetext="caption and comments"
+                    data-likes="0" data-dislikes="0" data-imagepath="img/img004.png" data-posturl="#">
+                </div>
+                <div class="expanded-col-1 expanded-carousel index4img" data-index="4" data-username="your-username" data-imagetext="caption and comments"
+                    data-likes="0" data-dislikes="0" data-imagepath="img/img005.png" data-posturl="#">
+                </div>
+                <div class="expanded-col-1 expanded-carousel index5img" data-index="5" data-username="your-username" data-imagetext="caption and comments"
+                    data-likes="0" data-dislikes="0" data-imagepath="img/img006.png" data-posturl="#">
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</body>
+
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/js-lightbox.js"></script>
+
+</html>
