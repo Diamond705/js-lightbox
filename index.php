9e86b7fca9476357dca02a0eb491a819919efad1
Diamond705 <n.diamond.705@gmail.com>
2017-07-08 16:14:37 +0100

    updated data attributes and modal display function

--- index.php
+++ index.php
@@ -20,17 +20,23 @@
         <div class="row">
             <div class="expanded-col-60">
 
-                <div class="expanded-col-1 expanded-carousel index0img" data-index="0" data-username="" data-imagetext="" data-location="" data-likes="" data-dislikes="" data-imagepath="" data-posturl="#">
+                <div class="expanded-col-1 expanded-carousel index0img" data-index="0" data-username="" data-imagetext="" data-location=""
+                    data-likes="" data-dislikes="" data-imagepath="" data-posturl="#">
                 </div>
-                <div class="expanded-col-1 expanded-carousel index1img">
+                <div class="expanded-col-1 expanded-carousel index1img" data-index="1" data-username="" data-imagetext="" data-location=""
+                    data-likes="" data-dislikes="" data-imagepath="" data-posturl="#">
                 </div>
-                <div class="expanded-col-1 expanded-carousel index2img">
+                <div class="expanded-col-1 expanded-carousel index2img" data-index="2" data-username="" data-imagetext="" data-location=""
+                    data-likes="" data-dislikes="" data-imagepath="" data-posturl="#">
                 </div>
-                <div class="expanded-col-1 expanded-carousel index3img">
+                <div class="expanded-col-1 expanded-carousel index3img" data-index="3" data-username="" data-imagetext="" data-location=""
+                    data-likes="" data-dislikes="" data-imagepath="" data-posturl="#">
                 </div>
-                <div class="expanded-col-1 expanded-carousel index4img">
+                <div class="expanded-col-1 expanded-carousel index4img" data-index="4" data-username="" data-imagetext="" data-location=""
+                    data-likes="" data-dislikes="" data-imagepath="" data-posturl="#">
                 </div>
-                <div class="expanded-col-1 expanded-carousel index5img">
+                <div class="expanded-col-1 expanded-carousel index5img" data-index="5" data-username="" data-imagetext="" data-location=""
+                    data-likes="" data-dislikes="" data-imagepath="" data-posturl="#">
                 </div>
 
             </div>
--- js/js-lightbox.js
+++ js/js-lightbox.js
@@ -1,12 +1,24 @@
-jQuery(function($){
-    var updateArrows = function(){
+//Determine arrow behaviour based on number of media items
+jQuery(function ($) {
+    var updateArrows = function () {
         $('.expanded-right').removeClass('disabled');
         $('.expanded-left').removeClass('disabled');
         var curIndex = $('.expanded-carousel.active').data('index');
-        updateArrows.nbrOfItems = updateArrows.nbrOfItems || $('.expanded-carousel').length -1;
+        updateArrows.nbrOfItems = updateArrows.nbrOfItems || $('.expanded-carousel').length - 1;
 
         curIndex === updateArrows.nbrOfItems && $('.expanded-right').addClass('disabled');
         curIndex === 0 && $('.expanded-left').addClass('disabled');
     };
+
+    //Display modal settings
+    $('.expanded-carousel').on('click', function (e) {
+        scrollTo = $('body').scrollTop();
+        $('body').addClass('noscroll');
+        $('body').css('position', 'fixed');
+        $('.expanded-col-1, .expanded-col-2').removeClass('active');
+        $(this).addClass('active');
+        showModal($(this));
+        updateArrows();
+    });
 
 });
