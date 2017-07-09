3355359680c58879e38c604a8c2fae2780b77843
Diamond705 <n.diamond.705@gmail.com>
2017-07-09 12:32:08 +0100

    function to remove modal on certain click events

--- js/js-lightbox.js
+++ js/js-lightbox.js
@@ -43,23 +43,23 @@ jQuery(function ($) {
                 modalHtml += "<div class='expanded-modal'><span class='expanded-left'><span class='icons icon-arrow-left6'></span></span><span class='expanded-right'><span class='icons icon-arrow-right6'></span></span>";
                 modalHtml += "<div class='container'>";
                 modalHtml += "<span class='icons iconscircle-cross close-icon'></span>";
-                modalHtml += "<div class='expanded-scrollbox' style='max-height:"+maxHeight+"px'><div class='expanded-modal-image'>";
-                modalHtml += "<img src='"+imagepath+"' alt='expanded image'>";
+                modalHtml += "<div class='expanded-scrollbox' style='max-height:" + maxHeight + "px'><div class='expanded-modal-image'>";
+                modalHtml += "<img src='" + imagepath + "' alt='expanded image'>";
                 modalHtml += "</div>";
                 modalHtml += "<div class='expanded-modal-text'>";
-                modalHtml += "<span class='expanded-modal-username'><a href='"+postURL+"'>"+username+"</a> </span>";
+                modalHtml += "<span class='expanded-modal-username'><a href='" + postURL + "'>" + username + "</a> </span>";
                 modalHtml += "<span class='expanded-item-modal-likes'>";
                 modalHtml += "<ul class='list-inline'>";
                 modalHtml += "<li>";
 
                 modalHtml += "<span class='icons icon-arrow-up2'></span>";
-                modalHtml += "<a href='"+postURL+"'>"+likes+"</a>";
+                modalHtml += "<a href='" + postURL + "'>" + likes + "</a>";
                 modalHtml += "</li>";
                 modalHtml += "<li>";
 
 
                 modalHtml += "<span class='icons icon-arrow-down2'></span>";
-                modalHtml += "<a href='"+postURL+"'>"+dislikes+"</a>";
+                modalHtml += "<a href='" + postURL + "'>" + dislikes + "</a>";
                 modalHtml += "</li>";
 
                 modalHtml += "</ul>";
@@ -67,13 +67,31 @@ jQuery(function ($) {
 
                 modalHtml += "</span>";
                 modalHtml += "<span class='expanded-modal-imagetext'>";
-                modalHtml += "<p>"+imagetext+"</p>";
+                modalHtml += "<p>" + imagetext + "</p>";
                 modalHtml += "</span></div></div></div></div></div>";
                 $('body').append(modalHtml).fadeIn(2500);
             }
         }
 
 
+    };
+
+    $('body').on('click', '.expanded-wrapper', function (e) {
+        if ($(e.target).hasClass('.expanded-wrapper')) {
+            removeModal();
+        }
+    });
+    $('body').on('click', '.expanded-modal .iconscircle-cross', function (e) {
+        removeModal();
+    });
+
+    var removeModal = function () {
+        $('body').find('.expanded-wrapper').remove();
+        $('body').removeClass('noscroll');
+        $('body').css('position', 'static');
+        $('body').animate({
+            scrollTop: scrollTo
+        }, 0);
     };
 
 });
