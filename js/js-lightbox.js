c087c6a816cfc6c5610b5800296b0e9555b6ed11
Diamond705 <n.diamond.705@gmail.com>
2017-07-09 21:08:56 +0100

    javascript syntax fix

--- js/js-lightbox.js
+++ js/js-lightbox.js
@@ -1,65 +1,84 @@
+jQuery(function($) {
 //Determine arrow behaviour based on number of media items
-jQuery(function ($) {
-    var updateArrows = function () {
+    var updateArrows = function(){
         $('.expanded-right').removeClass('disabled');
         $('.expanded-left').removeClass('disabled');
         var curIndex = $('.expanded-carousel.active').data('index');
-        updateArrows.nbrOfItems = updateArrows.nbrOfItems || $('.expanded-carousel').length - 1;
+        updateArrows.nbrOfItems = updateArrows.nbrOfItems || $('.expanded-carousel').length -1;
 
         curIndex === updateArrows.nbrOfItems && $('.expanded-right').addClass('disabled');
         curIndex === 0 && $('.expanded-left').addClass('disabled');
     };
 
     //Display modal settings
-    $('.expanded-carousel').on('click', function (e) {
+    $('.expanded-carousel').on('click', function(e){
         scrollTo = $('body').scrollTop();
-        $('body').addClass('noscroll');
-        $('body').css('position', 'fixed');
+       $('body').addClass('noscroll');
+       $('body').css('position', 'fixed');
         $('.expanded-col-1, .expanded-col-2').removeClass('active');
         $(this).addClass('active');
         showModal($(this));
         updateArrows();
     });
 
-    var modalHTML = '';
-    showModal = function (that) {
-        //   console.log(that);
+    $('body').on('click', '.expanded-right, .expanded-left', function(e){
+        if($(this).hasClass('disabled')) return;
+        var curIndex = $('.expanded-carousel.active').data('index');
+        var nextItemIndex = parseInt(curIndex+1);
+        if($(this).hasClass('expanded-left')){
+            nextItemIndex-=2;
+        }
+        var nextItem = $('.expanded-carousel[data-index='+nextItemIndex+']');
+       // console.log(nextItemIndex);
+        if(nextItem.length > 0){
+            $('.expanded-col-1, .expanded-col-2').removeClass('active');
+            $('body').find('.expanded-wrapper').remove();
+            showModal($(nextItem.get(0)));
+            nextItem.first().addClass('active');
+        }
+        updateArrows();
+    });
 
-        //Declare data variables
+    var modalHtml = '';
+    showModal = function(that){
+     //   console.log(that);
+     //Declare data variables
         var username = that.data('username'),
-            // location = that.data('location'),
-            imagetext = that.data('imagetext'),
-            likes = that.data('likes'),
-            dislikes = that.data('dislikes'),
-            imagepath = that.data('imagepath'),
-            expandedUrl = that.data('url');
-        postURL = that.data('posturl');
+        location = that.data('location'),
+        imagetext = that.data('imagetext'),
+        likes =  that.data('likes'),
+        dislikes = that.data('dislikes'),
+        imagepath = that.data('imagepath'),
+        expandedUrl = that.data('url');
+        postURL =  that.data('posturl');
 
-        maxHeight = $(window).height() - 100;
+        maxHeight = $(window).height()-100;
 
         if ($('.expanded-wrapper').length === 0) {
-            if (typeof imagepath !== 'undefined') {
+            if(typeof imagepath !== 'undefined') {
                 modalHtml = "<div class='expanded-wrapper'>";
                 modalHtml += "<div class='expanded-modal'><span class='expanded-left'><span class='icons icon-arrow-left6'></span></span><span class='expanded-right'><span class='icons icon-arrow-right6'></span></span>";
                 modalHtml += "<div class='container'>";
                 modalHtml += "<span class='icons iconscircle-cross close-icon'></span>";
-                modalHtml += "<div class='expanded-scrollbox' style='max-height:" + maxHeight + "px'><div class='expanded-modal-image'>";
-                modalHtml += "<img src='" + imagepath + "' alt='expanded image'>";
+                modalHtml += "<div class='expanded-scrollbox' style='max-height:"+maxHeight+"px'><div class='expanded-modal-image'>";
+                modalHtml += "<img src='"+imagepath+"' alt='expanded image'>";
                 modalHtml += "</div>";
                 modalHtml += "<div class='expanded-modal-text'>";
-                modalHtml += "<span class='expanded-modal-username'><a href='" + postURL + "'>" + username + "</a> </span>";
+                modalHtml += "<span class='expanded-modal-username'><a href='"+postURL+"'>"+username+"</a> </span>"
+                modalHtml += "<span class='expanded-modal-location'>"+location+"</span>";
                 modalHtml += "<span class='expanded-item-modal-likes'>";
                 modalHtml += "<ul class='list-inline'>";
                 modalHtml += "<li>";
 
+
                 modalHtml += "<span class='icons icon-arrow-up2'></span>";
-                modalHtml += "<a href='" + postURL + "'>" + likes + "</a>";
+                modalHtml += "<a href='"+postURL+"'>"+likes+"</a>";
                 modalHtml += "</li>";
                 modalHtml += "<li>";
 
 
                 modalHtml += "<span class='icons icon-arrow-down2'></span>";
-                modalHtml += "<a href='" + postURL + "'>" + dislikes + "</a>";
+                modalHtml += "<a href='"+postURL+"'>"+dislikes+"</a>";
                 modalHtml += "</li>";
 
                 modalHtml += "</ul>";
@@ -67,46 +86,41 @@ jQuery(function ($) {
 
                 modalHtml += "</span>";
                 modalHtml += "<span class='expanded-modal-imagetext'>";
-                modalHtml += "<p>" + imagetext + "</p>";
+                modalHtml += "<p>"+imagetext+"</p>";
                 modalHtml += "</span></div></div></div></div></div>";
                 $('body').append(modalHtml).fadeIn(2500);
             }
         }
-
-
     };
 
-    $('body').on('click', '.expanded-wrapper', function (e) {
-        if ($(e.target).hasClass('.expanded-wrapper')) {
+    $('body').on( 'click','.expanded-wrapper', function(e) {
+        if($(e.target).hasClass('.expanded-wrapper')){
             removeModal();
         }
     });
-    $('body').on('click', '.expanded-modal .iconscircle-cross', function (e) {
+    $('body').on('click', '.expanded-modal .iconscircle-cross', function(e){
         removeModal();
     });
 
-    var removeModal = function () {
+     var removeModal = function(){
         $('body').find('.expanded-wrapper').remove();
         $('body').removeClass('noscroll');
         $('body').css('position', 'static');
-        $('body').animate({
-            scrollTop: scrollTo
-        }, 0);
+        $('body').animate({scrollTop: scrollTo}, 0);
     };
 
     // Avoid break on small devices
-    var expandedScrollMaxHeight = function () {
+    var expandedScrollMaxHeight = function() {
         if ($('.expanded-scrollbox').length) {
-            maxHeight = $(window).height() - 100;
-            $('.expanded-scrollbox').css('max-height', maxHeight + 'px');
+            maxHeight = $(window).height()-100;
+            $('.expanded-scrollbox').css('max-height',maxHeight+'px');
         }
     }
-
-    $(window).resize(function () { // set event on resize
+    $(window).resize(function() { // set event on resize
         clearTimeout(this.id);
         this.id = setTimeout(expandedScrollMaxHeight, 100);
     });
-    document.onkeydown = function (evt) {
+    document.onkeydown = function(evt) {
         evt = evt || window.event;
         if (evt.keyCode == 27) {
             removeModal();
